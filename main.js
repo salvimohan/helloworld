f7a7ec17dcda4ad5e3c992a652f15486eff9cb69
Jai Lalawat <jailalawat@gmail.com>
2017-01-22 23:17:09 +0530

    Update the mainjs

--- main.js
+++ main.js
@@ -16,7 +16,22 @@ app.on('ready', () => {
 
 var platform = os.platform() + '_' + os.arch();
 var version = app.getVersion();
-console.log(platform);
-console.log(version);
+
 
 autoUpdater.setFeedURL('http://localhost:5000/update/'+platform+'/'+version);
+
+autoUpdater.on('update-availabe', () => {
+    console.log('update available')
+})
+autoUpdater.on('checking-for-update', () => {
+  console.log('checking-for-update')
+})
+autoUpdater.on('update-not-available', () => {
+  console.log('update-not-available')
+})
+autoUpdater.on('update-downloaded', (e) => {
+  console.log(e)
+  alert("Install?")
+    autoUpdater.quitAndInstall()
+})
+autoUpdater.checkForUpdates()
